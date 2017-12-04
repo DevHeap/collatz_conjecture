ccebaa31f3d3932c499aaaa851b6cd15024bc206
Aljutor <aljutor@gmail.com>
2017-12-04 18:31:26 +0300

    Allow any origin of websocket

--- src/backend/server/websocket.go
+++ src/backend/server/websocket.go
@@ -10,7 +10,9 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {return true},
+}
 
 
 func WSHandler(w http.ResponseWriter, r *http.Request) {
