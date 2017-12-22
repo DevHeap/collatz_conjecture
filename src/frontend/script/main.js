e0ce210bde98bc1786aad1499b9673d22bd6b712
Alexey Gayduk <le5h@ya.ru>
2017-12-22 09:16:23 +0300

    First release for public testing
    All test code deleted or disabled.
    Layout changed.
    Many refactoring =D
    UX improvements.
    Server address updated.
    New title!!!

--- src/frontend/script/main.js
+++ src/frontend/script/main.js
@@ -1,21 +1,20 @@
 Math.logRand = function(min, max){
     var range = Math.abs(max - min);
-    var scale = Math.ceil(Math.random() * Math.log10(range + 0.5));
+    var scale = Math.round(Math.random() * Math.log10(range + 0.5));
     var value = Math.floor(Math.random() * (range + 1));
     return Math.round(value / Math.pow(10.0, scale)) + min;
 }
 
-Math.isOdd = function(num){
-    return num % 2 ? true : false;
-} // Math.isEven = function(num){ return num % 2 ? false : true;} 
+// Math.isEven = function(num){ return num % 2 ? false : true;} 
+Math.isOdd = function(num){ return num % 2 ? true : false; }
 
 Math.collatzSequence = function(num){
-    var sequence = []; sequence.push(num);
+    var sequence = [num];
     while(num > 1){
         if(Math.isOdd(num)){ num = 3 * num + 1; }
         else{ num = num / 2; }
         sequence.push(num);
-    } // console.log(sequence);
+    } 
     return sequence;
 }
 
@@ -23,30 +22,25 @@ Math.collatzSequence = function(num){
 $(document).ready(function(){
 
     // server url (ws/wss)
-    var ws_url = 'wss://echo.websocket.org' ; // test server: 'wss://echo.websocket.org'
+    var ws_url = 'ws://devheap.org:8080/ws'; // test server: 'wss://echo.websocket.org'
     var ws; // global scope for websocket instance
     
-    var test_json = '{"Number":1,"PathLength":1,"MaxNumber":1,"AverageNumber":1,"Time":1}';
+    var minimum = 3; // 1 is done, 2 is only 1 step, 3 is okay.
+    var maximum = 4 * 8 * 15 * 16 * 23 * 42 * 108; // DHARMA INITIATIVE
     
     var $input = $('#collatzNumber'); // field with a number
     var $button = $('#collatzAction'); // two functional button
     
-    var $debug = $('#debugOutput'); // for messages
+    var $debug = $('#collatzDebug'); // for messages
     var $random = $('#collatzRandom'); // random button
     
-    var $exampleButton = $('#collatzExampleButton'); // example button
-    var $exampleResult = $('#collatzExampleResult'); // example button
-    
     var $results = $('#collatzResults'); // results block
-    var $answers = $('#collatzOutput'); // place to output results
     var $histogram = $('#collatzHistogram'); // place for histogram
+    var $answers = $('#collatzOutput'); // place to output results
     
-    var counter = 0; // answer counter
-    
-    var maximum = 4 * 8 * 15 * 16 * 23 * 42 * 108; // DHARMA INITIATIVE
-    var minimum = 3; // 1 is done, 2 is only 1 step, 3 is okay.
+    var $example = $('#collatzExample'); // example placeholder
 
-    // for collecting stats
+    // Histogram singleton for collecting stats
     Histogram = {
         values: [0], total: 0, maximum: 0,
         reset: function(){
@@ -66,147 +60,136 @@ $(document).ready(function(){
             }
         },
         print: function(){
-            var result = [];
-            for(var i = 0; i < this.values.length; i++){
-                if(this.values[i]>0){
-                    result.push(i+':'+this.values[i]);
-                }
-            }
-            return result.join(', ');
+            return this.values.reduce(function(a, b, i){
+                return a + ', ' + i + ':' + b;
+            });
         }
     };
     
-    function createDiv(content, id) {
+    // fast way to create element (in theory)
+    function createItem(content, id) {
         var elem = document.createElement('div');
         elem.classList.add("item"); elem.id = id;
         elem.appendChild(document.createTextNode(content));
         return elem;
     }
     
+    // answer counter
+    var counter = 0; 
+    
+    // adds answer
     function addAnswer(data){
-        counter++;
-        var result, maximum = 100, update = 10;
         
+        var result, itemid, maximum = 100;
+        
+        // prepare string
         if (typeof data === 'object'){
-            
-            Histogram.add(data['PathLength']);
-            
-            var elements = []; // prepare string (#GOVNOKOD)
-            elements.push('Number: ' + data['Number']);
-            elements.push('Length: ' + data['PathLength']);
-            elements.push('Highest: ' + data['MaxNumber']);
-            elements.push('Average: ' + data['AverageNumber']);
-            elements.push('Time: ' + (data['Time']/1000) + 'ms'); // is it really ms?
+            var elements = [
+                'Number: ' + data['Number'],
+                'Path length: ' + data['PathLength'],
+                'Highest: ' + data['MaxNumber'],
+                'Average: ' + data['AverageNumber'],
+                'Calc time: ' + (data['Time']/1000) + 'ms', // is it really ms?
+            ];
             result = elements.join(', ');
-            
+            itemid = 'number_' + data['Number'];
         } else {
             result = data;
-            
-            Histogram.add(Math.logRand(1,666)); // testing
-            // console.log(Histogram.values);
+            itemid = 'item_'+(counter++);
         }
         
-        if(Histogram.total % update == 0){
-            // $histogram.text(Histogram.print());
-            Plotly.relayout($histogram[0], {y: Histogram.values});
-        }
-            
-        $answers.queue(function(){ // experimental sh!t (that works)
+        // queue
+        $answers.queue(function(){
             // limit shown elements by removing oldest
             if(counter>=maximum) {
-                $(this).children('#answer_'+(counter-maximum)).queue(function(){
-                    $(this).remove().dequeue();
+                $(this).children().first().queue(function(){
+                    $(this).remove();
                 });
             }
-            
             // add result
-            // var item = '<div id="answer_'+counter+'" class="item">' + result + '</div>'; // create div instead?
-            var item = createDiv(result, 'answer_'+counter);
+            var item = createItem(result, itemid);
             $(this).append(item).ready(function(){
-                item.scrollIntoView({block: "end", behavior: "smooth"}); // smooth by css
+                // smooth scroll
+                item.scrollIntoView({block: "end", behavior: "smooth"}); // TODO: fix page scroll
             });
-                
+            // next please
             $(this).dequeue();
         });
+        
     }
     
+    // updates histogram
+    function updateHistogram(data){
+        var update = 3 * 7; // magic number
+        if (typeof data === 'object'){
+            Histogram.add(data['PathLength']);
+        } else {
+            // Histogram.add(Math.logRand(1,666)); // TESTing (histogram)
+        }
+        if(Histogram.total % update == 0){
+            Plotly.relayout($histogram[0], {y: Histogram.values});
+        }
+    }
+    
+    // onpage log
     function logDebug(data){
-        $debug.append(data+'\n').animate({scrollTop: $debug[0].scrollHeight}, 200, 'swing');
+        var dt = new Date();
+        var message = dt.getHours() + ':' + dt.getMinutes()+ ':' + dt.getSeconds() + ' - ' + data + '\n';
+        $debug.append(message).animate({scrollTop: $debug[0].scrollHeight}, 200, 'swing');
     }
     
-    $button.attr('data-opened', 'false');
-    
-    $random.on('click', function(ev){ $input.val(Math.logRand(minimum, maximum)); });
-    
-    $exampleButton.on('click', function(ev){
-        var value = $input.val();
-        var result = Math.collatzSequence(value);
-        $exampleResult.text('Path for '+ value +' is ' + result.join(' > '));
-    });
-    
-    $button.on('click', function(ev){
+    function doAction(){
         
         if($button.attr('data-opened') == 'false'){
             
             $button.attr('disabled','disabled').val('Connecting...');
             
-            $debug.html('');
-            
+            // clear output
             $answers.html('');
             
+            // histogram init
             Histogram.reset();
-            
-            // histo init
             Plotly.plot($histogram[0], [{y: Histogram.values, type: 'bar'}], {showlegend: false}, {staticPlot: true});
             
-            $results.slideDown(500);
+            counter = 0; // ugly...
             
-            //* for testing
-            var test = setInterval(function(){
-                var rand = Math.random()*3;
-                for(var i = 0; i < rand; i++){
-                    addAnswer('test '+counter);
-                }
-            },200);
-            //*/
-            
+            // new connection
+            ws = new WebSocket(ws_url);
             logDebug('Connection attempt');
-            ws = new WebSocket(ws_url); // new connection
-            
-            counter = 0;
             
             ws.onopen = function(){
-                // ws.send(test_json);
-                console.log('ws opened');
-                logDebug('Connection is opened');
+                logDebug('Connection is opened'); // console.log('ws opened');
+                
+                ws.send($input.val());
+                logDebug('Message sent: ' + $input.val());
                 
                 $button.attr('data-opened', 'true').removeAttr('disabled').val('Stop');
-                
-                var value = $input.val();
-                
-                ws.send(value);
-                logDebug('Message sent: "' + value + '"');
             };
             
             ws.onmessage = function(ev){
-                console.log('ws answer'); // if(counter==0) console.log(ev);
-                addAnswer($.parseJSON(ev.data));
+                // console.log('ws answer');
+                var data = $.parseJSON(ev.data);
+                $answers.queue(function(){ addAnswer(data); $(this).dequeue(); });
+                $histogram.queue(function(){ updateHistogram(data); $(this).dequeue(); });
             };
             
             ws.onerror = function(){
-                console.log('ws error');
-                logDebug('An error has occurred');
+                logDebug('An error has occurred'); // console.log('ws error');
             };
             
             ws.onclose = function(ev){
-                console.log('ws closed: ' + ev.code);
-                logDebug('Connection is closed (' + ev.code + ')');
+                logDebug('Connection is closed (' + ev.code + ')'); // console.log('ws closed: ' + ev.code);
                 
                 $button.attr('data-opened', 'false').removeAttr('disabled').val('Start');
-                // $input.val(Math.logRand(minimum, maximum)); // UX =)
-                
-                clearInterval(test); // testing
             };
+            
+            /* TESTing (answer adding)
+            var test = setInterval(function(){
+                var rand = Math.random()*10;
+                for(var i = 0; i < rand; i++){ addAnswer('test '+counter); }
+            }, 100);
+            setTimeout(function(){clearInterval(test)}, 10000);
+            //*/
             
         } else {
             
@@ -216,8 +199,46 @@ $(document).ready(function(){
             
         }
         
+    }
+    
+    function example(){
+        var value = $input.val();
+        var result = Math.collatzSequence(value);
+        $example.text('Path for '+ value +' is ' + result.join(' > '));
+    }
+    
+    $button.attr('data-opened', 'false');
+    
+    $button.on('click',  function(ev){
+        // show results block
+        $results.slideDown(500, function(){
+            // show debug output
+            $debug.slideDown(500); 
+        });
+        doAction();
+    });
+
+    $example.on('click',  function(ev){
+        $example.toggleClass('collapsed');
+    });
+    
+    $input.on('keyup', function(ev){
+        if(ev.keyCode == 13){ doAction(); }
+        $(this).val($(this).val().replace(/[^0-9]/g, ''));
+        example();
+    });
+    
+    $input.on('change', function(ev){
+        $(this).val($(this).val().replace(/[^0-9]/g, ''));
+        example();
+    });
+    
+    $random.on('click', function(ev){
+        $input.val(Math.logRand(minimum, maximum));
+        example();
     });
     
     $input.val(Math.logRand(minimum, maximum));
+    example();
     
 });
